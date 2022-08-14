8ee70ce1bd8e8461dd0e08ebb1994bc06c114746
Elan <elansakthivel2000@gmail.com>
2022-08-14 20:43:58 +0530

    test linting

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,30 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 function Counter({ initialCount = 0 }) {
-	// const { current: initialState } = useRef({ count: initialCount });
-	const [initialState] = React.useState({ count: initialCount });
-	// const [initialState] = React.useReducer((s) => s, { count: initialCount });
+    // const { current: initialState } = useRef({ count: initialCount });
+    const [initialState] = React.useState({ count: initialCount });
+    // const [initialState] = React.useReducer((s) => s, { count: initialCount });
 
-	const [count, setCount] = useState(initialCount);
+    const [count, setCount] = useState(initialCount);
 
-	const increment = () => {
-		setCount((count) => count + 1);
-	};
+    const increment = () => {
+        setCount((prevCount) => prevCount + 1);
+    };
 
-	const reset = () => {
-		setCount(initialState.count);
-	};
+    const reset = () => {
+        setCount(initialState.count);
+    };
 
-	return (
-		<div>
-			<button onClick={increment}>{count}</button>
-			<button onClick={reset}>Reset</button>
-		</div>
-	);
+    return (
+        <div>
+            <button type="button" onClick={increment}>
+                {count}
+            </button>
+            <button type="button" onClick={reset}>
+                Reset
+            </button>
+        </div>
+    );
 }
 
 export default Counter;
